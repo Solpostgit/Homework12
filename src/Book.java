ee585cf41a7d806044a9886805adde7f6754c7da
Solpostgit <begemot_off@mail.ru>
2023-09-05 22:21:04 +0300

    Add comments Не понятно как прописать класс Author в классе Book.

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,10 @@
 public class Book {
     private String bookName;
     private int publishingYear;
-    public Book(String bookName, int publishingYear) {
+    public Book(String bookName, int publishingYear) { //должно быть так, public Book(String bookName, int publishingYear, Author author) {
         this.bookName = bookName;
         this.publishingYear = publishingYear;
+        // не понятно, как здесь прописать класс Author
     }
     public String getBookName() {
         return this.bookName;
