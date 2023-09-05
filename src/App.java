a2bd40e594039237053ff0ea7882afbf96304c40
Solpostgit <begemot_off@mail.ru>
2023-09-05 21:19:45 +0300

    Rename class App > Book

--- src/App.java
+++
@@ -1,4 +0,0 @@
-public class App {
-    String name;
-    int yearPublishing;
-}
---
+++ src/Book.java
@@ -0,0 +1,7 @@
+public class Book {
+    String name;
+    int yearPublishing;
+    public Book(String name, int yearPublishing) {
+        this.name
+    }
+}
