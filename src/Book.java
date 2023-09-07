630d31d8cfc5fda7ab583f7d9b8a4b33dc85d1eb
Solpostgit <begemot_off@mail.ru>
2023-09-07 05:53:06 +0300

    1 Исправил ошибки в классе Author; 2 Исправил ошибки в классе Book; 3 Указал в классе Book, объект класса Author; 4 В классе Main, исправил ошибки в контрукторах Author, Book; 5 Проверил работу программы, программа работает.

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,26 @@
 public class Book {
     private String bookName;
     private int publishingYear;
-    public Book(String bookName, int publishingYear) { //должно быть так, public Book(String bookName, int publishingYear, Author author) {
+    Author author;
+
+    public Book(String bookName, int publishingYear, Author author) {
         this.bookName = bookName;
         this.publishingYear = publishingYear;
-        // не понятно, как здесь прописать класс Author
+        this.author = author;// прописал объект класса Author
     }
-    public String getBookName() {
+
+    public String getbookName() {
         return this.bookName;
     }
+
     public int getpublishingYear() {
         return this.publishingYear;
     }
+
+    Author getAuthor() {
+        return this.author;
+    }
+
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Author tolstoy = new Author("Lev", "Tolstoy");
         Author lermontov = new Author("Mihail", "Lermontov");
-        Book warAndPeace = new Book("War and Peace", 1950);
-        Book mciry = new Book("Mciry", 1955);
-        System.out.println("author = " + tolstoy.getName());
-        System.out.println("author = " + tolstoy.getSurname());
-        System.out.println("book = " + warAndPeace.getBookName());
-        System.out.println("book = " + warAndPeace.getpublishingYear());
-        warAndPeace.setPublishingYear(1951);
-        System.out.println("book = " + warAndPeace.getpublishingYear());
+        Book warAndPeace = new Book("War and Peace", 1950, tolstoy);
+        Book mciry = new Book("Mciry", 1955, lermontov);
+        System.out.println("author1 = " + tolstoy.getName());
+        System.out.println("author1 = " + tolstoy.getSurname());
+        System.out.println("book1 = " + warAndPeace.getbookName());
+        System.out.println("book1 = " + warAndPeace.getpublishingYear());
+        warAndPeace.setPublishingYear(1951); //изменяем год публикации, через сеттер
+        System.out.println("book1 = " + warAndPeace.getpublishingYear());
         System.out.println("author2 = " + lermontov.getName());
         System.out.println("author2 = " + lermontov.getSurname());
-        System.out.println("book2 = " + mciry.getBookName());
+        System.out.println("book2 = " + mciry.getbookName());
         System.out.println("book2 = " + mciry.getpublishingYear());
     }
 }
